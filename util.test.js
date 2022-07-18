ae21eab7634c2a70f73834353da6713c80171682
maste150hhu <mastremolten2@gmail.com>
2022-07-18 23:20:29 +0200

    pass third test

--- util.test.js
+++ util.test.js
@@ -7,3 +7,8 @@ test("always has length 2", () => {
 test("if treshold is zero then the first set is empty", () => {
     expect(splitDataset(0, [1,2,3])).toStrictEqual([[],[1, 2, 3]]);
 })
+
+test("both cardinalities together correspond to the cardinality of the first set", () => {
+    const [set1, set2] = splitDataset(0, [1, 2, 3]);
+    expect(set1.length + set2.length).toBe(3);
+})
