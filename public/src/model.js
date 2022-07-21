a5b0dd815fa985e59258d6c80a5df334679a77e7
maste150hhu <mastremolten2@gmail.com>
2022-07-21 16:33:06 +0200

    add data to the nn

--- public/src/model.js
+++ public/src/model.js
@@ -1,58 +1,61 @@
 let nn, inputDataWithoutMask, inputDataWithMask, images = [];
 
 const IMAGE_WIDTH = 128;
-  const IMAGE_HEIGHT = 128;
-  const IMAGE_CHANNELS = 4;
+const IMAGE_HEIGHT = 128;
+const IMAGE_CHANNELS = 4;
 
-  const options = {
-    epochs: 1024,
-    batchSize: 48,
-    layers: [
-        {
-          type: 'conv2d',
-          filters: 8,
-          kernelSize: 5,
-          strides: 1,
-          activation: 'relu',
-          kernelInitializer: 'varianceScaling',
-        },
-        {
-          type: 'maxPooling2d',
-          poolSize: [2, 2],
-          strides: [2, 2],
-        },
-        {
-          type: 'conv2d',
-          filters: 16,
-          kernelSize: 5,
-          strides: 1,
-          activation: 'relu',
-          kernelInitializer: 'varianceScaling',
-        },
-        {
-          type: 'maxPooling2d',
-          poolSize: [2, 2],
-          strides: [2, 2],
-        },
-        {
-          type: 'flatten',
-        },
-        {
+const options = {
+  layers: [
+      {
+        type: 'conv2d',
+        filters: 8,
+        kernelSize: 5,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling',
+      },
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2],
+      },
+      {
+        type: 'conv2d',
+        filters: 16,
+        kernelSize: 5,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling',
+      },
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2],
+      },
+      {
+        type: 'flatten',
+      },
+      {
+        type: 'dense',
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax',
+      },
+      {
           type: 'dense',
-          kernelInitializer: 'varianceScaling',
-          activation: 'softmax',
-        },
-        {
-            type: 'dense',
-            activation: 'sigmoid',
-            units: 64
-        }
-      ],
-    task: "imageClassification",
-    inputs:[IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    outputs: ['label'],
-    debug: true
-  }
+          activation: 'sigmoid',
+          units: 64
+      }
+    ],
+  task: "imageClassification",
+  inputs:[IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+  debug: true
+}
+
+const trainingOptions = {
+  epochs: 1024,
+  batchSize: 48
+}
+
 
 nn = ml5.neuralNetwork(options);
 
@@ -66,34 +69,59 @@ const getImage = async (path, label) => {
   const blob = await response.blob()
   const resourceUrl = URL.createObjectURL(blob)
 
-  images.push({image: window.loadImage(resourceUrl), label: label})  // this is p5.js loadImage
+  let image = window.loadImage(resourceUrl)
+  //images.push({image: image, label: label})
+
+  return {image: image, label: label}
 }
 
-async function performTraining(data) {
-  console.log('data loaded');
-
+function processImages(rawImages) {
+  const result = [];
   // label data
-  data.without_mask
+  rawImages.without_mask.slice(0,100)
     .forEach((image) => 
-      getImage(`/without_mask/${
-        (image.includes("simple")) ? "simple/" + image : "complex/" + image}`));
+      result.push(getImage(`/without_mask/${
+        (image.includes("simple")) ? "simple/" + image : "complex/" + image}`, "without_mask")));
 
-  data.with_mask
+  rawImages.with_mask.slice(0,100)
     .forEach((image) => 
-      getImage(`/with_mask/${
-        (image.includes("simple")) ? "simple/" + image : "complex/" + image}`));
+      result.push(getImage(`/with_mask/${
+        (image.includes("simple")) ? "simple/" + image : "complex/" + image}`, "with_mask")));
 
-  images.forEach(
-    (data) => {
-      nn.addData({image: data.image}, {label: data.label});
-    }
-  )
+  dataLoaded(result, rawImages);
+}
 
-  //nn.normalizeData();
+function preload() {
 
-  //nn.train(options, console.log("finished training"));
+  fetch("http://localhost:8080/images")
+    .then(result => {return result.json()})
+    .then(json => processImages(json))
+}
+
+function dataLoaded(loadedImages, rawImages) {
+
+  const withoutMaskFileNames = rawImages.without_mask;
+  const withMaskFileNames = rawImages.with_mask;
+  const allFiles = withoutMaskFileNames.concat(withMaskFileNames);
+
+  setTimeout(document.body.insertAdjacentHTML("afterbegin", loadedImages), 100)
+  console.log("loadedImages: " + loadedImages);
+  Promise.all(loadedImages).then(results => {
+    results.forEach((result,i) => {
+      console.log(`image ${i} loaded`)
+      nn.addData({image: result}, {label: (i < 100) ? "without_mask" : "with_mask"})
+      //image(result.image, 128, 128)
+    })
+    document.body.insertAdjacentHTML("afterbegin", results[0].image.loadPixels() + ", " + results[0].label + "<br>".repeat(2))
+    // train
+    //nn.addData({image: results[0].image, label: new Array(results[0].label)})
+    //document.body.insertAdjacentHTML("afterbegin", nn.data)
+    //nn.normalizeData();
+    //nn.train(trainingOptions);
+  })
+  
 }
 
 function setup() {
-  fetch("http://localhost:8080/images").then(result => {return result.json()}).then(json => performTraining(json));
+
 }
